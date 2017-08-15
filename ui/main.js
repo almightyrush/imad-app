c9e32ec38c3043e840b8065e0165bb6542a192db
almightyrush <rushabh.sojitra@gmail.com>
2017-08-15 14:11:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "New HTml Edited";
 var img = document.getElementById("rus");
-rus.onlclick = function(){
+rus.onlClick = function(){
   img.style.marginLeft = '100px';  
 };
