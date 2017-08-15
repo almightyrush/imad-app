1c22002fe67ba17a9315518d89ae0a039db594ba
almightyrush <rushabh.sojitra@gmail.com>
2017-08-15 14:08:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "New HTml Edited";
+var img = document.getElementById("img");
+img.onlclick = function(){
+  img.style.marginLeft = '100px';  
+};
