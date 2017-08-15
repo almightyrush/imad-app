4285bb182ffd5e70b3cb16e864eea3b250e7b8a4
almightyrush <rushabh.sojitra@gmail.com>
2017-08-15 13:53:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,11 +110,11 @@ app.get('/:articleName',function(req,res){
 
 /*app.get('/article-two',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });*/
+
+app.get('/ui/main.js',function(req,res){
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
