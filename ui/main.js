52f059ddc1d9b83c6d5f173be7acdde464dc9257
almightyrush <rushabh.sojitra@gmail.com>
2017-08-21 10:16:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,14 @@
-console.log('Loaded!');
-var element = document.getElementById("main-text");
-element.innerHTML = "New HTml Edited";
-var img = document.getElementById("rus");
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
+//Counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+    
+    //Make a request to the counter endpoint
+    
+    //Capture the response and store it in a variable 
+    
+    //render the variable in the correct span
+    counter = counter +1;
+    var span = document.getElementById('count');
+    span.innerHtml = counter.toString();
 }
-img.onclick = function(){
-    var interval =setInterval(moveRight,50);
-};
